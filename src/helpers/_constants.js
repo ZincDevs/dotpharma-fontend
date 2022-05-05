d85f8c8edf964917c9836943a7fca04017bc382a
rukundoeric <ericrukundo005@gmail.com>
2022-05-05 16:50:40 +0200

    update _constants.js

--- src/helpers/_constants.js
+++ src/helpers/_constants.js
@@ -1,8 +1,11 @@
 export default {
   t_key_name: 'dotpharma-key-c-u',
   login_api: '/user/login',
+  refresh_token_api: '/user/refresh-token',
   admin_login_api: '/user/admin/login',
   signup_api: '/user/signup',
+  logout_api: 'user/logout',
+  get_all_users_api: 'user/allusers',
   verify_user_api: token => `/user/activateuser/${token}`,
   resend_verification_api: '/user/resendverification',
 };
