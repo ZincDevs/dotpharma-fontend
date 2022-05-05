1cad7e3bf0bf15c5a1e27e4cc1a40d5a6b2e3a0f
rukundoeric <ericrukundo005@gmail.com>
2022-05-05 16:48:22 +0200

    update  auth

--- src/api/_auth.js
+++ src/api/_auth.js
@@ -1,13 +1,15 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable camelcase */
-import request from './_request';
+import axios from './_axios';
 import { Constants } from '../helpers';
 
-const { login_api, signup_api, admin_login_api } = Constants;
+const {
+  login_api, signup_api, admin_login_api, refresh_token_api,
+} = Constants;
 export const signUp = async user => {
   try {
     user.role = 'patient';
-    const data = await request('post', signup_api, null, user);
+    const { data } = await axios.post(signup_api, user);
     return { ...data, status: 'success' };
   } catch ({ response: { data } }) {
     return { ...data, status: 'fail' };
@@ -15,9 +17,14 @@ export const signUp = async user => {
 };
 export const logIn = async (auth, isAdmin) => {
   try {
-    const data = await request('post', isAdmin ? admin_login_api : login_api, null, auth);
+    const { data } = await axios.post(isAdmin ? admin_login_api : login_api, auth);
     return { ...data, status: 'success' };
   } catch ({ response: { data } }) {
     return { ...data, status: 'fail' };
   }
+};
+
+export const refreshToken = async () => {
+  const { data } = await axios.get(refresh_token_api);
+  return { ...data, status: 'success' };
 };
