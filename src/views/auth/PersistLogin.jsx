bdd828f82e80a86da92fd9405c0959170959da3c
rukundoeric <ericrukundo005@gmail.com>
2022-05-06 21:00:31 +0200

    update PersistLogin.jsx

--- src/views/auth/PersistLogin.jsx
+++ src/views/auth/PersistLogin.jsx
@@ -4,35 +4,30 @@ import { Outlet } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import useRefreshToken from '../../hooks/useRefreshToken';
 import useAuth from '../../hooks/useAuth';
-import Empty from '../shared/Empty';
+import Splash from '../shared/Splash';
 
 function PersistLogin() {
   const [isLoading, setIsLoading] = useState(true);
   const refresh = useRefreshToken();
   const { auth } = useAuth();
 
-  useEffect(() => {
-    const verifyRefreshToken = async () => {
-      try {
-        await refresh();
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setIsLoading(false);
-        // navigate('/login', { state: { from: location }, replace: true });
-      }
-    };
-    !auth.access_token ? verifyRefreshToken() : setIsLoading(false);
-  }, []);
+  const stopLoading = () => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  };
 
   useEffect(() => {
-    console.log(`isLoading ${isLoading}`);
-    console.log(`accessToken ${auth?.access_token}`);
-  }, [isLoading]);
+    const verifyRefreshToken = async () => {
+      await refresh();
+      stopLoading();
+    };
+    !auth.access_token ? verifyRefreshToken() : stopLoading();
+  }, []);
 
   return (
     <>
-      {isLoading ? <Empty /> : <Outlet />}
+      {isLoading ? <Splash /> : <Outlet />}
     </>
   );
 }
