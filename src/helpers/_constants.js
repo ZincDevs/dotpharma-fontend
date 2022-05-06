b17fabb8f15688095a0440971cfd43620daca50c
rukundoeric <ericrukundo005@gmail.com>
2022-05-06 21:01:28 +0200

    fix some errors

--- src/helpers/_constants.js
+++ src/helpers/_constants.js
@@ -5,7 +5,9 @@ export default {
   admin_login_api: '/user/admin/login',
   signup_api: '/user/signup',
   logout_api: 'user/logout',
-  get_all_users_api: 'user/allusers',
+  get_all_users_api: '/user/allusers',
+  r_psw_rese_api: '/user/request-password-reset',
+  r_r_psw_rese_api: '/user/resend-password-reset',
   verify_user_api: token => `/user/activateuser/${token}`,
   resend_verification_api: '/user/resendverification',
 };
