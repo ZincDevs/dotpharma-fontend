d2a0307e912615db63fbb4a9a16d5bce8cf2ab69
rukundoeric <ericrukundo005@gmail.com>
2022-05-06 21:01:58 +0200

    update RequireAuthAdmin.jsx

--- src/views/routes/RequireAuthAdmin.jsx
+++ src/views/routes/RequireAuthAdmin.jsx
@@ -1,14 +1,12 @@
-import { useLocation, Navigate, Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
 
 function RequireAuth() {
   const { auth } = useAuth();
-  // console.log(auth);
-  const location = useLocation();
   return (
     auth?.access_token && auth?.userData?.u_role === 'SUPER_ADMIN'
       ? <Outlet />
-      : <Navigate to="/unauthorized" state={{ from: location }} replace />
+      : <Navigate to="/" />
   );
 }
 
