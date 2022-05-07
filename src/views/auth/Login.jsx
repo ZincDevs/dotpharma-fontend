30920c8f60010c3e6181b9ddc073ccf4ba8146d4
rukundoeric <ericrukundo005@gmail.com>
2022-05-07 22:42:37 +0200

    update Login.jsx

--- src/views/auth/Login.jsx
+++ src/views/auth/Login.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /* eslint-disable camelcase */
 /* eslint-disable react/prop-types */
 /* eslint-disable react/no-unescaped-entities */
@@ -71,37 +72,40 @@ function Login({ auth: { loginResponse }, logInAction }) {
     <div className="loginContainer">
       <div className="row loginContent">
         <div className="col-12 right d-flex justify-content-center align-items-center">
-          <div className="c-f-content">
-            {loginResponse.status === 'pending' && (<ProgressBar />)}
-            <div className="c-f-i-content py-4 px-5">
-              <ContentHead label="Sign In 🤞" />
-              <div className="c-content-fields w-auto">
-                <form onSubmit={handleLogin}>
-                  <Email
-                    handleOnChange={handleEmailChange}
-                    value={email}
-                    errors={emailErrors}
-                    labeled
-                  />
-                  <Password
-                    handleOnChange={handlePasswordChange}
-                    value={password}
-                    errors={passwordErrors}
-                  />
-                  <TCPRemember />
-                  <div className="c-c-link px-3 py-2 d-flex flex-row-reverse w-auto">
-                    <Link to="/login/password-reset">Forgot password?</Link>
-                  </div>
-                  <Button label="Sign In" classes="primary-button" />
-                  <Line label="Or Sign In with Email" />
-                  <GoogleBtn />
-                </form>
-              </div>
-              <div className="f-c-link-b w-auto py-3 d-flex justify-content-center align-items-center">
-                <div className="d-flex flex-row">
-                  <span className="px-1">Don&apos;t an account? </span>
-                  <Link to="/signup">Sign Up</Link>
+          <div className="c-f-u-content">
+            <ContentHead />
+            <div className="c-f-content">
+              {loginResponse.status === 'pending' && (<ProgressBar />)}
+              <div className="c-f-i-content py-4 px-5">
+                <div className="c-content-fields w-auto">
+                  <h6>Sign In 🤞</h6>
+                  <form onSubmit={handleLogin}>
+                    <Email
+                      handleOnChange={handleEmailChange}
+                      value={email}
+                      errors={emailErrors}
+                      labeled
+                    />
+                    <Password
+                      handleOnChange={handlePasswordChange}
+                      value={password}
+                      errors={passwordErrors}
+                    />
+                    <TCPRemember />
+                    <div className="c-c-link px-3 py-2 d-flex flex-row-reverse w-auto">
+                      <Link to="/forgot-password">Forgot password?</Link>
+                    </div>
+                    <Button label="Sign In" classes="primary-button" />
+                    <Line label="Or" />
+                    <GoogleBtn />
+                  </form>
                 </div>
+              </div>
+            </div>
+            <div className="f-c-link-b w-auto py-3 d-flex justify-content-center align-items-center">
+              <div className="d-flex flex-row">
+                <span className="px-1">Don&apos;t an account? </span>
+                <Link to="/signup">Sign Up</Link>
               </div>
             </div>
           </div>
