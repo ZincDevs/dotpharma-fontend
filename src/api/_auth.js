df1280877ee7f6d82ea7929e5f015dd4bd7eb2ec
rukundoeric <ericrukundo005@gmail.com>
2022-05-08 23:46:39 +0200

    update _auth.js

--- src/api/_auth.js
+++ src/api/_auth.js
@@ -4,27 +4,41 @@ import axios from './_axios';
 import { Constants } from '../helpers';
 
 const {
-  login_api, signup_api, admin_login_api, refresh_token_api,
+  login_api,
+  signup_api,
+  // admin_login_api,
+  refresh_token_api,
+  google_auth_api,
 } = Constants;
-export const signUp = async user => {
+
+export const signUp = async (user, callback) => {
   try {
-    user.role = 'patient';
     const { data } = await axios.post(signup_api, user);
-    return { ...data, status: 'success' };
-  } catch ({ response: { data } }) {
-    return { ...data, status: 'fail' };
+    callback(null, data);
+  } catch (error) {
+    callback(error);
   }
 };
-export const logIn = async (auth, isAdmin) => {
+
+export const logIn = async (auth, callback) => {
   try {
-    const { data } = await axios.post(isAdmin ? admin_login_api : login_api, auth);
-    return { ...data, status: 'success' };
-  } catch ({ response: { data } }) {
-    return { ...data, status: 'fail' };
+    const { data } = await axios.post(login_api, auth);
+    callback(null, data);
+  } catch (error) {
+    callback(error);
   }
 };
 
 export const refreshToken = async () => {
   const { data } = await axios.get(refresh_token_api);
   return { ...data, status: 'success' };
+};
+
+export const googleAuth = async (info, callback) => {
+  try {
+    const { data } = await axios.post(google_auth_api, info);
+    callback(null, data);
+  } catch (error) {
+    callback(error);
+  }
 };
