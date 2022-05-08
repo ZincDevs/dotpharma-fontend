fe76ed725b8d881e75cff866c77c899618b233d6
rukundoeric <ericrukundo005@gmail.com>
2022-05-08 23:46:59 +0200

    update VerificationFailed.jsx

--- src/views/auth/VerificationFailed.jsx
+++ src/views/auth/VerificationFailed.jsx
@@ -1,23 +1,22 @@
-/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 /* eslint-disable max-len */
-import React, { useEffect, useState } from 'react';
-import { connect } from 'react-redux';
-import { resentVerificationAction } from '../../redux/actions';
+import React, { useState } from 'react';
 import { Button, ProgressBar } from '../shared/Elements';
+import { resentVerification } from '../../api';
 import {
   Email,
 } from '../shared/Input';
 import Loader from '../shared/Loader';
 import { Validate } from '../../helpers';
 import VerificationSent from './VerificationSent';
+import Alert from '../shared/Alert';
 
-function VericationFailed({
-  user: { verifyResponse, resendVerificationResponce },
-  resentVerificationAction,
-}) {
+function VericationFailed({ alert: defaultAlert }) {
   const [email, setEmail] = useState();
   const [emailErrors, setEmailErrors] = useState(null);
+  const [status, setStatus] = useState();
+  const [alert, setAlert] = useState(defaultAlert);
+  const [showAlert, setShowAlert] = useState(true);
   const canContinue = !!(!emailErrors && email);
   const handleEmailChange = e => {
     setEmail(e.target.value);
@@ -27,31 +26,42 @@ function VericationFailed({
   const ValidateInputs = () => {
     handleEmailChange({ target: { value: email } });
   };
+  const handleCloseAlert = () => {
+    setShowAlert(false);
+  };
+  const handleShowAlert = data => {
+    setAlert(data || defaultAlert);
+    setShowAlert(true);
+  };
   const handleResend = e => {
     e.preventDefault();
     if (!canContinue) {
       ValidateInputs();
     } else {
-      resentVerificationAction(email);
+      setStatus('pending');
+      resentVerification(email, err => {
+        if (err) {
+          setStatus('fail');
+          const resScode = err?.response?.status;
+          if (resScode === 400 || resScode === 409) {
+            setEmailErrors(err?.response.data && [{ ...err.response.data.error.email }]);
+          } else {
+            handleShowAlert({ type: 'err', message: 'Something went wrong. please try again latter' });
+          }
+        } else {
+          setStatus('success');
+        }
+      });
     }
   };
-  useEffect(() => {
-    switch (resendVerificationResponce.status) {
-      case 'fail': {
-        setEmailErrors(resendVerificationResponce.error.email && [{ ...resendVerificationResponce.error.email }]);
-        break;
-      }
-      default:
-    }
-  }, [resendVerificationResponce]);
 
-  if (resendVerificationResponce.status === 'success') {
+  if (status === 'success') {
     return (<VerificationSent email={email} />);
   }
   return (
     <div className="empty-container d-flex flex-column email-sent">
 
-      {resendVerificationResponce.status === 'pending' ? (
+      {status === 'pending' ? (
         <div>
           <ProgressBar />
           <div className="empty-content email-sent d-flex flex-column">
@@ -60,6 +70,7 @@ function VericationFailed({
         </div>
       ) : (
         <div className="empty-content email-sent d-flex p-3">
+          {(showAlert && alert) && (<Alert info={alert} handleCloseAlert={handleCloseAlert} />)}
           <div className="v-c-failed p-4 d-flex flex-column text-center justify-content-center align-items-center">
             <h1 className="error"><i className="bi bi-envelope-exclamation" /></h1>
             <h3 className="text-1 mt-3">Verification failed 😥</h3>
@@ -82,8 +93,4 @@ function VericationFailed({
   );
 }
 
-const mapStateToProps = ({ user }) => ({
-  user,
-});
-
-export default connect(mapStateToProps, { resentVerificationAction })(VericationFailed);
+export default VericationFailed;
