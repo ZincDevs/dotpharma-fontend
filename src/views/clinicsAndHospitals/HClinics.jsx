c61ee32bd866edae4a7826512110c52ce5f0c659
rukundoeric <ericrukundo005@gmail.com>
2022-05-14 02:55:40 +0200

    feat: create HClinics.jsx

---
+++ src/views/clinicsAndHospitals/HClinics.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import Empty from '../shared/Empty';
+
+export default function HClinics() {
+  return (
+    <div className="clinics-container">
+      <Empty />
+    </div>
+  );
+}
