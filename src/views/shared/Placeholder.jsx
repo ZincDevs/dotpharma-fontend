aeef0d77ab422e668afc47504c3aa4c31e0c26cd
rukundoeric <ericrukundo005@gmail.com>
2022-05-14 02:58:54 +0200

    feat: create Placeholder.jsx

---
+++ src/views/shared/Placeholder.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { ThreeDots } from 'react-loader-spinner';
+
+export function ProductPlaceholder(props) {
+  return (
+    <div className="product p-4 d-flex justify-content-center align-items-center">
+      <div className="mt-3">
+        <ThreeDots
+          color="#F5F5F5"
+          height="60"
+          width="100"
+        />
+      </div>
+    </div>
+  );
+}
+
+export function CartPlaceholder(props) {
+  return (
+    <div>Placeholder</div>
+  );
+}
